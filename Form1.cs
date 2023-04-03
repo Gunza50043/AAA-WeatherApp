9b8e80c6f5b1256eb649ce206d3d5cc6a11774e4
Добрый Александр <saschaomel4uk@yandex.ru>
2023-04-03 10:45:10 +0700

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherForm : Form
+    {
+        private WeatherDatabase _weather;
+
+        public WeatherForm()
+        {
+            InitializeComponent();
+
+            _weather = new WeatherDatabase();
+            _weather.Initialize();
+
+            WeatherDataGrid.DataSource = _weather.WeatherList.ToList();
+        }
+
+
+        WeatherDatabase WDB = new WeatherDatabase();
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            string InputText = textBox1.Text;
+            WeatherDataGrid.DataSource = WDB.searchWeathers(WDB.WeatherList, InputText);
+
+
+/*
+            string inputText = textBox1.Text;
+            var IText = WDB.WeatherList.Where(T => T.CityName == inputText)
+                                    .Select(T => T)
+                                    .ToList();
+*/
+                           // WeatherDataGrid.DataSource = IText.ToList();
+
+        }
+    }
+}
---
+++ WeatherDatabase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeatherApp
+{
+    public class WeatherDatabase
+    {
+        private Random _random = new Random();
+        private int RandomInt
+        {
+            get => _random.Next(-30, 61);
+        }
+        internal List<Weather> WeatherList { get; private set; }
+
+        public void Initialize()
+        {
+            WeatherList = new List<Weather>
+            {
+                new Weather(RandomInt, MeasureUnits.Celsius, new City("Абакан")),
+                new Weather(RandomInt, MeasureUnits.Celsius, new City("Черногорск")),
+                new Weather(RandomInt, MeasureUnits.Fahrenheit, new City("Минусинск")),
+                new Weather(RandomInt, MeasureUnits.Kelvin, new City("Сорск")),
+                new Weather(RandomInt, MeasureUnits.Celsius, new City("Абаза")),
+                new Weather(RandomInt, MeasureUnits.Celsius, new City("Аскиз")),
+                new Weather(RandomInt, MeasureUnits.Kelvin, new City("Боград")),
+                new Weather(RandomInt, MeasureUnits.Celsius, new City("Таштып")),
+                new Weather(RandomInt, MeasureUnits.Celsius, new City("Копьёво"))
+            };
+        }
+        internal object searchWeathers (List<Weather> WaetherList, string InputText)
+        {
+            var weatherList = WeatherList.Where(T => T.CityName == InputText)
+                .Select(T => T)
+                .ToList();
+            return weatherList;
+
+        }
+    }
+}
